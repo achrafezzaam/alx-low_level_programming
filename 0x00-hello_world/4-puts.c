1314e374540e0a6a711b2ba34d3e1bac6b0ca7fd
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-16 13:31:33 +0100

    Betty coding style proof program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
-int main(){ 
+/**
+ * main - Prints message and returns 0
+ *
+ * Description: Print the following message using puts
+ * '\"Programming is like building a multilingual puzzle'
+ * and return the value 0
+ * Return: 0
+ */
+int main(void) { 
 	puts("\"Programming is like building a multilingual puzzle"); 
-	return 0; 
+	return (0); 
 }
