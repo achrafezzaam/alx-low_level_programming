99485e8f93c957b24aa4a35f954cf4e9cbf87c72
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-16 13:42:36 +0100

    5-printf.c program uses printf to print a message with an ending return to the line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints message and returns 0
+ *
+ * Description: Print the following message using printf
+ * '\"Programming is like building a multilingual puzzle'
+ * and return the value 0
+ * Return: 0
+ */
+int main(void)
+{ 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
