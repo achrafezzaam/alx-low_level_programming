2616c717bd7a6a43298a7585c023b0759fed5853
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-16 14:32:03 +0100

    removing the \ before - elements

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
 /**
  * main - Prints a message without using printf or puts
  * Return: 1
  */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19\n";
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19n";
 	long l = 14;
 	long fd = 1;
 	long syscall = 1;
