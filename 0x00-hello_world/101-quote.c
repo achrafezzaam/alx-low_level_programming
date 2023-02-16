af808fba2b81bac726b4d25ec495a49ad9e10749
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-16 14:37:45 +0100

    Betty coding style proof program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,14 @@
 int main(void)
 {
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19n";
-	long l = 14;
+	long l = 59;
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
+
 	__asm__("syscall"
-		:"=a" (ret)
-		:"a" (syscall),
+		: "=a" (ret)
+		: "a" (syscall),
 		"D" (fd),
 		"S" (s),
 		"d" (l)
