2b4eda7a94a540a28046539e229b486ebd4b2b8f
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-17 13:00:26 +0100

    5-print_numbers.c program done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print numbers from 0 to 9
+ * 
+ * Description: Print all the one digit integers
+ * from 0 to 9 and adding a return to the line at the end
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	do {
+		printf("%d", i);
+		if (i == 9)
+		{
+			printf("\n");
+		}
+		i++;
+	} while (i < 10);
+	return (0);
+}
