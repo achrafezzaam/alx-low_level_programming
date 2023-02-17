63daebefcd9189cbaad6a41ee3ef3ea9ac46048a
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-17 18:16:57 +0100

    changing the logic operator in the if state

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,43 +9,35 @@
  */
 int main(void)
 {
-	int i = 48;
-
+	int x = 0;
+	
 	do {
-		int j = 48;
+		int y = x + 1;
 
 		do {
-			int k = i;
+			int i , j, k, l = 0;
 
-			do {
-				int l = j;
-
-				if (i <= k)
-				{
-					l = j + 1;
-				}
-				do {
-					putchar(i);
-					putchar(j);
-					putchar(32);
-					putchar(k);
-					putchar(l);
-					if (i != 57 || j != 56 || k != 57 || l != 57)
-					{
-						putchar(44);
-						putchar(32);
-					}
-					else
-					{
-						putchar('\n');
-					}
-					l++;
-				} while (l < 58);
-				k++;
-			} while (k < 58);
-			j++;
-		} while (j < 57);
-		i++;
-	} while (i < 58);
+			i = (int) x / 10;
+			j = (int) x % 10;
+			k = (int) y / 10;
+			l = (int) y % 10;
+			putchar(i);
+			putchar(j);
+			putchar(32);
+			putchar(k);
+			putchar(l);
+			if (x != 98 || y != 99)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			else
+			{
+				putchar('\n');
+			}
+			y++;
+		} while (y < 100)
+		x++;
+	} while (x < 99);
 	return (0);
 }
