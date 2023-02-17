7420e2f07802c4af676a237d19db447a6c9aa893
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-17 12:45:38 +0100

    renaming the last_digit varible to last to reduce the number of characters

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,24 +11,24 @@
 int main(void)
 {
 	int n;
-	int last_digt;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digt = n % 10;
-	if (last_digt > 5)
+	last = n % 10;
+	if (last > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digt);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
 	else
 	{
-		if (last_digt == 0)
+		if (last == 0)
 		{
-			printf("Last digit of %d is %d and is 0\n", n, last_digt);
+			printf("Last digit of %d is %d and is 0\n", n, last);
 		}
 		else
 		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digt);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 		}
 	}
 	return (0);
