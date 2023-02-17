5f73389f2d10691a374991096936fcbf15c070e5
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-17 15:58:04 +0100

    8-print_base16 since I don't need to switch a number from decimal to hexadecimal I just added 39 to the counter

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Print all the numbers from 0 to f
+ *
+ * Description: Using putchar to print all
+ * the one digit numbers from 0 to f
+ * Retrun: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	do {
+		putchar(i);
+		i++;
+	} while (i < 58);
+	do {
+		putchar(i+39);
+		if (i == 63)
+		{
+			putchar('\n');
+		}
+		i++;
+	} while(i < 64);
+	return (0);
+}
