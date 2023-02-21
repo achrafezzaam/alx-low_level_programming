34bd682fa3932ac5c5182413fd64411032319a99
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-21 10:59:56 +0100

    testing the function _putchar using a header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * main - Write the message "_putchar"
+ *
+ * Description: Mes use of the _putchar function
+ * to print the message "_putchar" without any use of the
+ * standard library.
+ * Return: On success 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
