b8e065bd71264fd786833badbd9c3005a8561f3a
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-27 11:23:39 +0100

    Creating the different tasks files and creating the 0-reset_to_98.c program. Which resets the pointed varible value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - Reset the given input to the value 98
+ *
+ * Description: takes a pointer as a parameter
+ * and sets the value it points to to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
