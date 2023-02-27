157f510753ae3a92cf838111332d748af725684a
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-27 12:46:26 +0100

    First test of the 4 first programs

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,6 @@
 /**
  * reset_to_98 - Reset the given input to the value 98
+ * @n: the pointer to the variable to reset
  *
  * Description: takes a pointer as a parameter
  * and sets the value it points to to 98
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swap the values of a and b
+ * @a: first value to swap
+ * @b: second value to swap
+ *
+ * Description: take two pointers as parameters
+ * and swaps the values pointed by them
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
