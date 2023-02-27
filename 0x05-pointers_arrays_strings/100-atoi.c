c4e865979811270244fabdcc51d20e29d649f081
achrafezzaam <achraf.ezzaam@gmail.com>
2023-02-27 16:47:12 +0100

    First test of the 100-atoi.c program. Might get an error for the negative numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+ * _atoi - converts a string to int
+ * @s: the string to convert
+ *
+ * Description: take a pointer as parameter
+ * and converts the variable it points to to an int
+ * Return: return the int conversion or 0
+ * if the string doesn't have a number in it
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
