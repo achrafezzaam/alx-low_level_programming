ec98eb8ac1a1bd101e371fc6dbd6963e4edb4935
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-02 14:30:38 +0100

    Print a buffer b

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
+ *
+ * Description: Print a buffer 10 bytes at a time, starting with
+ * the byte position, then showing the conten's hex value,
+ * then display the 10 charcaters.
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if ((j + i) >= size)
+				printf("  ");
+
+			else
+				printf("%02x", *(b + j + i));
+
+			if ((j % 2) != 0 && j != 0)
+				printf(" ");
+		}
+
+		for (j = 0; j < 10; j++)
+		{
+			if ((j + i) >= size)
+				break;
+
+			else if (*(b + j + i) >= 31 &&
+				 *(b + j + i) <= 126)
+				printf("%c", *(b + j + i));
+
+			else
+				printf(".");
+		}
+
+		if (i >= size)
+			continue;
+
+		printf("\n");
+	}
+
+	if (size <= 0)
+		printf("\n");
+}
