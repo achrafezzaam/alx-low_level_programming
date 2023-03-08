c1b061cda55a55addb5b80a91b1cc9f07f2cbd79
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-08 11:42:02 +0100

    test task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string
+ * @s: strign to print
+ *
+ * Description: prints the string pointed to by
+ * s using the stdio labrary.
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef ALX_HEADER
+#define ALX_HEADER
+
+int _putchar(char);
+void _puts_recursion(char *s);
+
+#endif
