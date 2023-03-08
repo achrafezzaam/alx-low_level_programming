7098cb62bda4d8607e12f2b3ea96c4492ebae4d3
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-08 17:28:01 +0100

    Advanced taks done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		count++;
+		count += _strlen(s + count);
+	}
+
+	return (count);
+}
+
+/**
+ * check_pal - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int check_pal(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_pal(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int is_palindrome(char *s)
+{
+	int index = 0;
+	int len = _strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (check_pal(s, len, index));
+}
