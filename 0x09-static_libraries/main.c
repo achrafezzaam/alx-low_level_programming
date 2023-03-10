1ab2ec9ceaee7ae563316fcefaff0d43ff7e4c6b
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-10 13:50:20 +0100

    .

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include "main.h"
+
 int main(void)
 {
-	printf("Hello, World!");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
