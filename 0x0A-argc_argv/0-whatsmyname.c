e0f1fb5393fe1fe742d710eb47bc01d6691a8bc9
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-10 15:33:34 +0100

    .

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,10 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: Always o.
+ * Return: Always 0.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
