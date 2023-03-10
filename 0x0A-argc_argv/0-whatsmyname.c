57dc4eff1877c558973ee0895b1df698e46de16c
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-10 20:17:42 +0100

    .

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "main.h"
 /**
  * main - Prints the program name, followed by a new line.
  * @argc: The number of arguments supplied to the program.
@@ -5,8 +7,9 @@
  *
  * Return: return 0.
  */
-int main(int __attribute__((unused))  argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,7 @@
 #define FILDE_MAIN
 
 int _putchar(char c);
-int main(int __attribute__((unused)) argc, char *argv[]);
+int main(int argc, char *argv[]);
 int _atoi(char *s);
 
 #endif
