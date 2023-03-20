79163c6a240a32a4f1f6a1636e75f44f0618fccd
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-20 14:20:04 +0000

    The header file 4-sum.h defines a function like macro called SUM that coputes the sum of two numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef ALX_HEADER
+#define ALX_HEADER
+
+/*
+ * File: 4-sum.h
+ * Auth: Ezzaam Achraf
+ * Description: Create a function-like macro SUM(x, y)
+ *       that computes the sum of teh numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
