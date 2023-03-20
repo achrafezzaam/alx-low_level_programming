c54bcf21547564db688cd98ac9fa7eb31fef68d2
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-20 16:15:18 +0000

    Creating the header file for the alx 0x0E. C - Structures, typedef project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef ALX_HEADER
+#define ALX_HEADER
+
+/*
+ * File: dog.h
+ * Auth: Ezzaam Achraf
+ * Description: Header file that defines
+ * the structure dog.
+ */
+
+/*
+ * struct dog - Type describing a dog.
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog owner's name
+ *
+ * Description: Create a new data type called dog
+ * with the three parameters: name, age and owner.
+ */
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
