a0dc3eb31c2703c5f2f4dc3ed75e9a9ef8452835
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-20 10:51:21 +0000

    Creating a header file that defines a macro called PI wish will be an abbreviation for the token 3.14159265359

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef ALX_HEADER
+#define ALX_HEADER
+
+/*
+ * File: 1-pi.h
+ * Author: Ezzaam Achraf
+ * Description: creating a macro called
+ * PI as an abbreviation for the token 3.14159265359
+ */
+
+#define PI 3.14159265359
+
+#endif
