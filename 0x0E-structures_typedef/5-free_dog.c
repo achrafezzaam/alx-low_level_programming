9d0c40323b2c8ef4986def3bd1f72c6cadd72110
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-20 18:14:49 +0000

    Adding the function description

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
 #include "dog.h"
+
+/**
+ * free_dog - Free the memory space allocated to the
+ * struct dog
+ * @d: pointer to the struct dog to free
+ *
+ * Description: Make a check to see if d is NULL
+ * if not it will free the memory space of the name and owner
+ * first then the d memory space.
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
