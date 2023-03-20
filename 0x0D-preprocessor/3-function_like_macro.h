dec16186aaea0a185656896f08dbb8fa14917c86
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-20 11:06:52 +0000

    The 3-function_like_macro.h header file defines a function like macro called ABS(x) that computes the absolute value of a given variable x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef ALX_HEADER
+#define ALX_HEADER
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Ezzaam Achraf
+ * Description: create a function-like macro ABS(x)
+ * that computes the absolute value of a number x
+ */
+
+#define ABS(x) ((x) > 0 ? (x) : (-x))
+
+#endif
