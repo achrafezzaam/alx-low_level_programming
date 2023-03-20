065184833535dd9b5ee031628398dc0cd5b05f04
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-20 10:47:08 +0000

    Commenting the 0-object_like_macro.h file macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,13 @@
 #ifndef ALX_HEADER
 #define ALX_HEADER
 
-#define SIZE 1024;
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Ezzaam Achraf
+ * Description: Creation of a macro named
+ * SIZE as an abbreviation for the token 1024
+ */
+
+#define SIZE 1024
 
 #endif
