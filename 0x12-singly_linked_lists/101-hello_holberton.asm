ef12548f76b998cfd577a7b9549946b3b20bcad4
achrafezzaam <achraf.ezzaam@gmail.com>
2023-03-30 17:49:17 +0000

    Using the printf function to print the message

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,21 @@
-section .data
-	hello: db "Hello, Holberton", 10
-	hellolen: equ $-hello
+extern printf
 
 section .text
-	global _start
+   global main
 
-	_start:
-		mov rax, 1
-		mov rdi, 1
-		mov rsi, hello
-		mov rdx, hellolen
-		syscall
+main:
+   push rbp
 
-		mov rax,60
-		mov rdi, 0
-		syscall
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
