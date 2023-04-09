6221fdcb94e5f2ae6c5cc17300cc85a1d13006d7
achrafezzaam <achraf.ezzaam@gmail.com>
2023-04-09 21:41:37 +0000

    Correcting typos

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (len; text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 	o = open(filename, O_WRONLY | O_APPEND);
