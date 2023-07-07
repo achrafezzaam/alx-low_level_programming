eecd9f894268b6ae02e170e3c879b1b0f5460ba4
achrafezzaam <achraf.ezzaam@gmail.com>
2023-07-07 16:59:04 +0100

    Adding a check for NULL parameters

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *buff;
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL || key == '/0' || value == NULL)
+		return (0);
 	index = key_index((unsigned const char *)key, ht->size);
 	buff = ht->array[index];
 	new = malloc(sizeof(hash_node_t));
